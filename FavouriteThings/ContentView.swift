fdcac8db2364af0c70275cedceb9c8e0085e68e4
System Administrator <root@192-168-1-110.tpgi.com.au>
2020-04-26 18:22:30 +1000

    set up app

---
+++ FavouriteThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteThings
+//
+//  Created by mac on 24/4/20.
+//  Copyright © 2020 Wendy Yu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
