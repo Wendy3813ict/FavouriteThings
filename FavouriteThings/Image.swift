efd22bfb0edbe57e3a4768f12719b1c70961883b
System Administrator <root@192-168-1-110.tpgi.com.au>
2020-05-03 20:43:07 +1000

    complete set up image.swift

--- FavouriteThings/Image.swift
+++ FavouriteThings/Image.swift
@@ -8,3 +8,30 @@
 
 import Foundation
 import SwiftUI
+
+// return image to downloadedImage
+func downloadedImage (_ img: String)-> Image {
+
+    //  Check the url link is correct or not, if wrong url link will retrun the default image.
+    guard let imageURL = URL(string: img )
+        else {
+            return (Image("default"))
+    }
+
+    guard let imageData = try? Data(contentsOf: imageURL)
+        else {
+            print("Unsuccessful image download")
+            return (Image("default"))
+    }
+
+    guard let uiImage = UIImage(data: imageData) else {
+            print("Data not include image")
+            return (Image("default"))
+    }
+
+    // UIImage data will transfer Image and stored in the downloadedImage.
+    let downloadedImage = Image(uiImage: uiImage)
+    
+    // Return to the downloaded image
+    return downloadedImage
+}
